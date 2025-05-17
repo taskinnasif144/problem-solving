18899bce63c98872d2fe39ec894442616f086c93
taskinnasif144 <taskinnasif144@gmail.com>
2025-05-18 00:33:12 +0600

    asd

--- problem.c
+++ problem.c
@@ -3,27 +3,32 @@
 
 int main()
 {
-    int n, k;
-    scanf("%d %d", &n, &k);
-    int a[n];
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &a[i]);
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
     }
 
-     for (int i = 0; i < n; i++) {
-         for (int j = i + 1; j < n; j++) {
-            if(a[i] < a[j]) {
-                int tmp = a[i];
-                a[i] = a[j];
-                a[j] = tmp;
+
+
+    for (int i = 0; i < n -1; i++) {
+          int r = i+1;
+        if(arr[i] == 0){
+            while( arr[r] == 0 && r < n-1) {
+                r++;
             }
-         }
-     }
-    int sum = 0;
-    for (int i = 0; i < k; i++) {
-        sum += a[i];
+            int temp;
+            temp = arr[r];
+            arr[r] = arr[i];
+            arr[i] = temp;
+        }  
     }
 
-    printf("%d", sum);
     
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,25 +1,2 @@
-#include <stdio.h>
 
-int summation(int arr[], int size, int index) {
-    if(index == size) {
-        return 0;
-    }
-
-    return arr[index] + summation(arr, size, index + 1);
-}
-
-int main()
-{
-    int N;
-    scanf("%d", &N);
-    int A[N];
-    
-    for (int i = 0; i < N; i++) {
-        scanf("%d", &A[i]);
-    }
-    
-    int sum = summation(A, N, 0);
-    printf("%d", sum);
-
-    return 0;
-}
+        printf("%d", arr[i]);
