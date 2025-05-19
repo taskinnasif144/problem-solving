7267f48469b0fa1f4efaaa0571a8b53475c0de7d
taskinnasif144 <taskinnasif144@gmail.com>
2025-05-19 13:24:18 +0600

    solved an problem that finds the third number in a multiplication equation

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,2 +1,41 @@
+#include <stdio.h>
 
-        printf("%d", arr[i]);
+int main()
+{
+    int tests;
+    scanf("%d", &tests);
+    double arr[4];
+
+    for (int i = 0; i < tests; i++)
+    {
+        // to fill out the array
+        for (int i = 0; i < 4; i++)
+        {
+            scanf("%lf", &arr[i]);
+        }
+
+        double mulOf3 = arr[1] * arr[2] * arr[3];
+        double multiRes = arr[0];
+        if (arr[1] == 0 || arr[2] == 0 || arr[3] == 0) {
+            if (multiRes == 0) {
+                printf("0\n");
+            } else {
+                printf("-1\n");
+            }
+            continue;
+        }
+
+        double res = multiRes / mulOf3;
+
+        if (res == floor(res))
+        {
+            printf("%d", (int)res);
+        }
+        else
+        {
+            printf("-1");
+        }
+
+        printf("\n");
+    }
+}
