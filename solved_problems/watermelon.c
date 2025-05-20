b13988430db89a55f188996bc8b5eb3c3e4fd5a2
taskinnasif144 <taskinnasif144@gmail.com>
2025-05-20 21:52:11 +0600

    solved the watermelon problem

---
+++ solved_problems/watermelon.c
@@ -0,0 +1,19 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/223206/problem/B
+
+#include <stdio.h>
+
+int main()
+{
+    int w;
+    scanf("%d", &w);
+
+    if (w >= 4 && w % 2 == 0) {
+        printf("Yes");
+
+    } else {
+        printf("No");
+    }
+
+   
+    
+}
