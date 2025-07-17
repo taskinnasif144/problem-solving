94faae0be5f831c8cc25e53cede56226628d22c2
taskinnasif144 <taskinnasif144@gmail.com>
2025-07-18 00:33:37 +0600

    solved

---
+++ solved_problems/easy_problem.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int test;
+    cin >> test;
+    while(test--) {
+        int N;
+        cin >> N;
+        cout << N-1 << endl;
+    }
+    return 0;
+}
